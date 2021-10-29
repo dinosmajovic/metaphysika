8b77353c30e5d95a879ed362de0867267410d9f8
Emir Seremet <seremetemir1@gmail.com>
2021-10-29 18:06:38 +0300

    finished size guide responsiveness

--- src/views/Size-Guide/index.js
+++ src/views/Size-Guide/index.js
@@ -1,89 +1,157 @@
 import {
+  TablesWrapper,
+  TableContainer,
   Table,
   Container,
-  ShoesTable,
-  Description,
-  ClothesTable,
+  Hr,
   Text
 } from './styled';
+
+const shoeSizes = [
+  {
+    euro: '36',
+    cm: '22.5'
+  },
+  {
+    euro: '37',
+    cm: '23'
+  },
+  {
+    euro: '38',
+    cm: '23.5'
+  },
+  {
+    euro: '39',
+    cm: '24'
+  },
+  {
+    euro: '40',
+    cm: '25'
+  },
+  {
+    euro: '41',
+    cm: '26'
+  },
+  {
+    euro: '42',
+    cm: '26.5'
+  }
+];
+
+const clothingSizes = [
+  {
+    size: 'XS',
+    bust: '85',
+    waist: '67',
+    hips: '95'
+  },
+  {
+    size: 'S',
+    bust: '90',
+    waist: '72',
+    hips: '100'
+  },
+  {
+    size: 'M',
+    bust: '95',
+    waist: '77',
+    hips: '105'
+  },
+  {
+    size: 'L',
+    bust: '100',
+    waist: '82',
+    hips: '110'
+  },
+  {
+    size: 'XL',
+    bust: '105',
+    waist: '87',
+    hips: '115'
+  },
+  {
+    size: '2XL',
+    bust: '110',
+    waist: '92',
+    hips: '120'
+  },
+  {
+    size: '3XL',
+    bust: '117.5',
+    waist: '99.5',
+    hips: '127.5'
+  }
+];
 
 const SizeGuide = () => {
   return (
     <Container>
-      <ShoesTable>
-        <Description>Shoes are graded using Euro size only</Description>
-        <Table>
-          <tr>
-            <td>Euro</td>
-            <td>36</td>
-            <td>37</td>
-            <td>38</td>
-            <td>39</td>
-            <td>40</td>
-            <td>41</td>
-            <td>42</td>
-          </tr>
-          <tr>
-            <td>cm</td>
-            <td>22.5</td>
-            <td>23</td>
-            <td>23.5</td>
-            <td>24</td>
-            <td>25</td>
-            <td>26</td>
-            <td>26.5</td>
-          </tr>
-        </Table>
-      </ShoesTable>
-      <ClothesTable>
-        <Description>Clothing</Description>
-        <Table>
-          <tr>
-            <td>Size</td>
-            <td>XS</td>
-            <td>S</td>
-            <td>M</td>
-            <td>L</td>
-            <td>XL</td>
-            <td>2XL</td>
-            <td>3XL</td>
-          </tr>
-          <tr>
-            <td>Bust</td>
-            <td>85</td>
-            <td>90</td>
-            <td>95</td>
-            <td>100</td>
-            <td>105</td>
-            <td>110</td>
-            <td>117.5</td>
-          </tr>
-          <tr>
-            <td>Waist</td>
-            <td>67</td>
-            <td>72</td>
-            <td>77</td>
-            <td>82</td>
-            <td>87</td>
-            <td>92</td>
-            <td>99.5</td>
-          </tr>
-        </Table>
-        <Text>
-          <span>
-            One size fits all sizes due to fabric elasticity and/or clothes type
-          </span>
-          <ul>
-            <span>
-              All measurements are shown in CM/centimetres. When taking
-              measurements, please note:
-            </span>
-            <li>Bust: Measure around fullest part including normal bra.</li>
-            <li>Waist: Measure around the natural waistline.</li>
-            <li>Waist: Measure around the natural waistline.</li>
-          </ul>
-        </Text>
-      </ClothesTable>
+      <TablesWrapper>
+        <TableContainer>
+          <span>Shoes</span>
+          <Hr />
+          <Table>
+            <thead>
+              <tr>
+                <th>Euro</th>
+                <th>Cm</th>
+              </tr>
+            </thead>
+            <tbody>
+              {shoeSizes.map((size) => {
+                return (
+                  <tr key={size.euro}>
+                    <td>{size.euro}</td>
+                    <td>{size.cm}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </TableContainer>
+        <TableContainer>
+          <span>Clothing</span>
+          <Hr />
+          <Table>
+            <thead>
+              <tr>
+                <th>Size</th>
+                <th>Bust</th>
+                <th>Waist</th>
+                <th>Hips</th>
+              </tr>
+            </thead>
+            <tbody>
+              {clothingSizes.map((c) => {
+                return (
+                  <tr key={c.size}>
+                    <td>{c.size}</td>
+                    <td>{c.bust}</td>
+                    <td>{c.waist}</td>
+                    <td>{c.hips}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </TableContainer>
+      </TablesWrapper>
+      <Text>
+        <span>
+          One size fits all sizes due to fabric elasticity and/or clothes type
+        </span>
+        <span>
+          All measurements are shown in CM/centimetres. When taking
+          measurements, please note:
+        </span>
+        <ul>
+          <li>Bust: Measure around fullest part including normal bra.</li>
+          <li>Waist: Measure around the natural waistline.</li>
+          <li>
+            Hips: Measure 20cm down from natural waistline at the fullest part.
+          </li>
+        </ul>
+      </Text>
     </Container>
   );
 };
--- src/views/Size-Guide/styled.js
+++ src/views/Size-Guide/styled.js
@@ -4,57 +4,103 @@ import fonts from 'assets/fonts';
 
 export const Container = styled.div`
   width: 100%;
+
+  margin: 0px auto;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 20px 20px;
+`;
+
+export const TablesWrapper = styled.div`
+  display: flex;
+
+  @media (max-width: 600px) {
+    justify-self: center;
+    flex-direction: column-reverse;
+  }
+`;
+
+export const TableContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid ${colors.gray.light};
+
+  > span {
+    align-self: center;
+    justify-content: center;
+    font-size: 20px;
+    padding: 10px;
+  }
+
+  :first-child {
+    margin-right: 30px;
+
+    @media (max-width: 600px) {
+      align-items: center;
+      margin-right: 0px;
+    }
+  }
+
+  @media (max-width: 600px) {
+    margin-top: 20px;
+    margin-right: 0px;
+    width: 100%;
+  }
 `;
 
 export const Table = styled.table`
-  > tr {
+  text-align: left;
+
+  margin: 10px 0px;
+  padding: 10px;
+
+  > thead > tr {
+    > th {
+      color: ${colors.pink.primary};
+      font-size: 20px;
+      padding: 0px 10px;
+
+      @media (max-width: 600px) {
+        font-size: 18px;
+      }
+    }
+  }
+
+  > tbody > tr {
     > td {
-      padding: 30px;
-      font-weight: ${fonts.sfPro.fontWeight.semibold};
+      font-size: 18px;
+      padding: 10px;
+
+      @media (max-width: 600px) {
+        font-size: 16px;
+      }
     }
   }
 `;
 
-export const ShoesTable = styled.div`
-  padding: 20px;
-
-  > span {
-    font-weight: ${fonts.sfPro.fontWeight.semibold};
-  }
-`;
-
-export const Description = styled.span`
-  padding: 20px;
-  font-size: 20px;
-  color: ${colors.pink.primary};
-`;
-
-export const ClothesTable = styled.div`
-  padding: 20px;
-
-  > span {
-    font-weight: ${fonts.sfPro.fontWeight.semibold};
-  }
+export const Hr = styled.div`
+  width: 100%;
+  border-bottom: 1px solid ${colors.gray.light};
 `;
 
 export const Text = styled.div`
+  width: fit-content;
+  justify-self: center;
+  align-self: center;
+  margin-top: 20px;
+
   > span {
     display: block;
-    padding: 20px;
     font-weight: ${fonts.sfPro.fontWeight.semibold};
+
+    :not(:last-child) {
+      margin-bottom: 10px;
+    }
   }
 
   > ul {
-    padding: 10px 20px;
-
-    > span {
-      display: block;
-      margin-bottom: 10px;
-      font-weight: ${fonts.sfPro.fontWeight.semibold};
-    }
+    padding: 20px;
 
     > li {
       :not(:last-child) {
