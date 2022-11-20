eee104535213952895a3bc468241ed9fdd87eca3
Emir Seremet <seremetemir1@gmail.com>
2022-11-20 09:46:11 +0300

    Remove home page error when images are not uploaded correctly

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -26,15 +26,10 @@ const Home = () => {
 
   const getImages = async () => {
     setIsLoading(true);
-    try {
-      const { data } = await axios.get(API + '/homeImages');
-      const images = data.resources;
-      setImages(images);
-      setIsLoading(false);
-    } catch (error) {
-      history.push('/404');
-      setIsLoading(false);
-    }
+    const { data } = await axios.get(API + '/homeImages');
+    const images = data.resources;
+    setImages(images);
+    setIsLoading(false);
   };
 
   const goToPage = (path) => {
@@ -51,48 +46,48 @@ const Home = () => {
 
   return (
     <Wrapper>
-      <Top onClick={() => goToPage(images?.[6].context.custom.path)}>
-        <MainImg url={images?.[6].url} />
-        <Button type="top">{images?.[6].context.custom.caption}</Button>
+      <Top onClick={() => goToPage(images?.[6]?.context?.custom?.path)}>
+        <MainImg url={images?.[6]?.url} />
+        <Button type="top">{images?.[6]?.context?.custom?.caption}</Button>
       </Top>
       <Middle>
         <MiddleItem
-          url={images?.[5].url}
-          onClick={() => goToPage(images?.[5].context.custom.path)}
+          url={images?.[5]?.url}
+          onClick={() => goToPage(images?.[5]?.context?.custom?.path)}
         >
-          <Button type="small">{images?.[5].context.custom.caption}</Button>
+          <Button type="small">{images?.[5]?.context?.custom?.caption}</Button>
         </MiddleItem>
         <MiddleItem
-          url={images?.[4].url}
-          onClick={() => goToPage(images?.[4].context.custom.path)}
+          url={images?.[4]?.url}
+          onClick={() => goToPage(images?.[4]?.context?.custom?.path)}
         >
-          <Button type="small">{images?.[4].context.custom.caption}</Button>
+          <Button type="small">{images?.[4]?.context?.custom?.caption}</Button>
         </MiddleItem>
         <MiddleItem
-          url={images?.[3].url}
-          onClick={() => goToPage(images?.[3].context.custom.path)}
+          url={images?.[3]?.url}
+          onClick={() => goToPage(images?.[3]?.context?.custom?.path)}
         >
-          <Button type="small">{images?.[3].context.custom.caption}</Button>
+          <Button type="small">{images?.[3]?.context?.custom?.caption}</Button>
         </MiddleItem>
         <MiddleItem
-          url={images?.[2].url}
-          onClick={() => goToPage(images?.[2].context.custom.path)}
+          url={images?.[2]?.url}
+          onClick={() => goToPage(images?.[2]?.context?.custom?.path)}
         >
-          <Button type="small">{images?.[2].context.custom.caption}</Button>
+          <Button type="small">{images?.[2]?.context?.custom?.caption}</Button>
         </MiddleItem>
       </Middle>
       <Bottom>
         <BottomItem
-          url={images?.[1].url}
-          onClick={() => goToPage(images?.[1].context.custom.path)}
+          url={images?.[1]?.url}
+          onClick={() => goToPage(images?.[1]?.context?.custom?.path)}
         >
-          <Button>{images?.[1].context.custom.caption}</Button>
+          <Button>{images?.[1]?.context?.custom?.caption}</Button>
         </BottomItem>
         <BottomItem
-          url={images?.[0].url}
-          onClick={() => goToPage(images?.[0].context.custom.path)}
+          url={images?.[0]?.url}
+          onClick={() => goToPage(images?.[0]?.context?.custom?.path)}
         >
-          <Button>{images?.[0].context.custom.caption}</Button>
+          <Button>{images?.[0]?.context?.custom?.caption}</Button>
         </BottomItem>
       </Bottom>
     </Wrapper>
